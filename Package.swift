80ddbe4cbfc2a276a8ebe9321888efc0b98b0c03
millyguitron <millyguitron@outlook.com>
2022-08-13 17:23:40 -0700

    Updated dependencies

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/WeTransfer/Mocker.git", .upToNextMajor(from: "2.3.0")),
-        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "4.0.0"),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.4.0")),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", branch: "master"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", branch: "master"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
